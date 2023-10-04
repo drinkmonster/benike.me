7a74cfdcc3dc130071d5970ee20cc22dae98c132
drinkmonster <berenyibence98@gmail.com>
2023-10-04 12:08:58 +0200

    asd

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
-        <h1>Blog</h1>
+        <div class="flex flex-row justify-between p-3">
+            <NuxtLink to="/" class="text-4xl font-black">
+                benike.me
+            </NuxtLink>
+            <Icon class="mr-2 ml-2" name="pixelarticons:moon" size="48px" />
+        </div>
+        <h1 class="text-center text-4xl font-black">Blog</h1>
     </div>
 </template>
 
@@ -8,6 +14,4 @@
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,38 @@
 <template>
-    <div class="flex flex-col h-screen items-center justify-center">
+    <div class="flex flex-col h-screen items-center justify-center" :class="{ 'dark': isDarkMode }">
         <div class="flex flex-row text-center">
-            <h1 class="font-black text-4xl p-4">benike.me</h1>
+            <h1 :style="{ color: isDarkMode ? 'black' : 'white' }" class="font-black text-4xl p-4">benike.me</h1>
         </div>
         <div class="flex flex-row justify-center">
             <NuxtLink to="/blog">
-                <Icon class="mr-2 ml-2" name="pixelarticons:avatar" color="white" size="48px" />
+                <Icon class="mr-2 ml-2" :class="{ 'dark:text-white': isDarkMode }" name="pixelarticons:avatar" :color="isDarkMode ? 'black' : 'white'" size="48px" />
             </NuxtLink>
             <a href="https://github.com/drinkmonster">
-                <Icon class="mr-2 ml-2" name="pixelarticons:git-branch" color="white" size="48px" />
+                <Icon class="mr-2 ml-2" :class="{ 'dark:text-white': isDarkMode }" name="pixelarticons:git-branch" :color="isDarkMode ? 'black' : 'white'" size="48px" />
             </a>
-            <Icon class="mr-2 ml-2" name="pixelarticons:moon" color="white" size="48px" />
+            <button class="mr-2 ml-2" @click="toggleDarkMode">
+                <Icon :name="isDarkMode ? 'pixelarticons:sun' : 'pixelarticons:moon'" :color="isDarkMode ? 'black' : 'white'" size="48px" />
+            </button>
         </div>
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+const isDarkMode = ref(false);
+
+function toggleDarkMode() {
+  isDarkMode.value = !isDarkMode.value;
+}
 
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.dark {
+  background-color: #FFFFFF;
+}
+.dark .dark\\:text-white {
+  color: white;
+}
 </style>
