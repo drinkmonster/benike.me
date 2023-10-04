52c15ba2bab23cd97093ab3243b4a8c7395749ad
drinkmonster <berenyibence98@gmail.com>
2023-10-04 22:23:45 +0200

    simple search

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,9 +2,10 @@
     <Head>
         <Title>benike.me | Blog</Title>
     </Head>
-    <div>
+    <div class="flex flex-col justify-center">
         <h1 class="text-center text-4xl font-black pb-4">Blog</h1>
-        <div class="flex justify-center" v-for="post in posts">
+        <input class="self-center p-4" type="text" placeholder="Search..." v-model="search">
+        <div class="flex justify-center" v-for="post in filteredPosts">
             <BlogCard :post="post" />
         </div>
     </div>
@@ -12,11 +13,18 @@
 
 <script setup>
 import PocketBase from 'pocketbase';
+import { ref, computed } from 'vue';
 
 const pb = new PocketBase('https://benike.me/api');
 const posts = await pb.collection('posts').getFullList({
     sort: '-created',
 });
+const search = ref('');
+
+const filteredPosts = computed(() => {
+    return posts.filter(post => post.title.toLowerCase().includes(search.value.toLowerCase()));
+});
+
 definePageMeta({
     layout: 'blog',
 })
